fddcf826d1d2c3078453fcb701d9ba909779551d
ShakibProgrammar <shakib501886@gmail.com>
2025-03-13 10:41:08 +0600

    assignment done

---
+++ bin/homeworkAssignment/assignment10/main.dart
@@ -0,0 +1,4 @@
+// Assignment 10: Object Equality and HashCode
+// Create a class Person with properties name and age.
+// Override the == operator and hashCode getter to compare Person objects based on their name and age.
+// Test the equality and hash code by creating two Person objects with the same name and age and verifying that they are considered equal.
---
+++ bin/homeworkAssignment/assignment9/main.dart
@@ -0,0 +1,10 @@
+// Assignment 9: Callable Objects
+// Create a class Adder that implements the call() method to add two integers.
+// Test the class by creating an instance of Adder and calling it like a function (e.g., adder(3, 5) should return 8).
+
+import 'adder.dart';
+
+void main() {
+  Adder adder = Adder();
+  print(adder(3, 5));
+}
